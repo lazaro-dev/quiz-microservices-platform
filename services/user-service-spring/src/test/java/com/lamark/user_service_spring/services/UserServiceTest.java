1641d8bafb9b88b93cee71b115b793bb7284385a
Lázaro Marques <lazaro.sobral@facape.br>
2026-04-03 18:05:06 -0300

    fix: import unnecessary user service and test

--- services/user-service-spring/src/test/java/com/lamark/user_service_spring/services/UserServiceTest.java
+++ services/user-service-spring/src/test/java/com/lamark/user_service_spring/services/UserServiceTest.java
@@ -4,12 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
