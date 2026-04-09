a057346dd9ecab200ff243ab2f02b00bdc40589e
Lázaro Marques <lazaro.sobral@facape.br>
2026-04-08 22:43:50 -0300

    fix(user-service): test params incorrect

--- services/user-service-spring/src/test/java/com/lamark/user_service_spring/services/UserServiceTest.java
+++ services/user-service-spring/src/test/java/com/lamark/user_service_spring/services/UserServiceTest.java
@@ -30,146 +30,147 @@ import com.lamark.user_service_spring.infrastructure.repositories.UserRepository
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
-    @Mock
-    private UserRepository userRepository;
+        @Mock
+        private UserRepository userRepository;
 
-    @Mock
-    private PasswordEncoder passwordEncoder;
+        @Mock
+        private PasswordEncoder passwordEncoder;
 
-    @InjectMocks
-    private UserService userService;
+        @InjectMocks
+        private UserService userService;
 
-    @Test
-    @DisplayName("Deve criar um novo usuário com sucesso")
-    void createUserCase1() {
-        CreateUserDTO requestBody = new CreateUserDTO("Paulo", "paula@gmail", "password");
+        @Test
+        @DisplayName("Deve criar um novo usuário com sucesso")
+        void createUserCase1() {
+                CreateUserDTO requestBody = new CreateUserDTO("Paulo", "paula@gmail", "password");
 
-        when(passwordEncoder.encode(any()))
-                .thenReturn("hashedPassword");
+                when(passwordEncoder.encode(any()))
+                                .thenReturn("hashedPassword");
 
-        when(userRepository.save(any(User.class)))
-                .thenAnswer(invocation -> {
-                    User user = invocation.getArgument(0);
-                    user.setId(1L);
-                    return user;
-                });
+                when(userRepository.save(any(User.class)))
+                                .thenAnswer(invocation -> {
+                                        User user = invocation.getArgument(0);
+                                        user.setId(1L);
+                                        return user;
+                                });
 
-        UserDTO result = userService.createUser(requestBody);
+                UserDTO result = userService.createUser(requestBody);
 
-        assertNotNull(result);
-        assertEquals(1L, result.id());
-        assertEquals("Paulo", result.username());
+                assertNotNull(result);
+                assertEquals(1L, result.id());
+                assertEquals("Paulo", result.username());
 
-    }
+        }
 
-    @Test
-    @DisplayName("Deve falhar ao criar um novo usuário com email duplicado")
-    void createUserCase2() {
-        when(userRepository.existsByEmail("paula@gmail"))
-                .thenReturn(true);
+        @Test
+        @DisplayName("Deve falhar ao criar um novo usuário com email duplicado")
+        void createUserCase2() {
+                when(userRepository.existsByEmail("paula@gmail"))
+                                .thenReturn(true);
 
-        assertThrows(EmailAlreadyExistsException.class,
-                () -> userService.createUser(new CreateUserDTO("Paulo", "paula@gmail", "password")));
-    }
+                assertThrows(EmailAlreadyExistsException.class,
+                                () -> userService.createUser(new CreateUserDTO("Paulo", "paula@gmail", "password")));
+        }
 
-    @Test
-    @DisplayName("Deve atualizar usuário")
-    void updateUserCase1() {
+        @Test
+        @DisplayName("Deve atualizar usuário")
+        void updateUserCase1() {
 
-        User user = new User();
-        user.setId(1L);
-        user.setUsername("antonio");
-        user.setRole(UserRole.USER);
+                User user = new User();
+                user.setId(1L);
+                user.setUsername("antonio");
+                user.setRole(UserRole.USER);
 
-        when(userRepository.findById(1L))
-                .thenReturn(Optional.of(user));
+                when(userRepository.findById(1L))
+                                .thenReturn(Optional.of(user));
 
-        UpdateUserDTO dto = new UpdateUserDTO("antonio_andrade", null);
+                UpdateUserDTO dto = new UpdateUserDTO("antonio_andrade", null);
 
-        UserDTO result = userService.updateUser(1L, dto);
+                UserDTO result = userService.updateUser(1L, dto, UserRole.ADMIN.name());
 
-        assertEquals("antonio_andrade", result.username());
-    }
+                assertEquals("antonio_andrade", result.username());
+        }
 
-    @Test
-    @DisplayName("Deve falhar ao atualizar usuário com email duplicado")
-    void updateUserCase2() {
+        @Test
+        @DisplayName("Deve falhar ao atualizar usuário com email duplicado")
+        void updateUserCase2() {
 
-        User user = new User();
-        user.setId(1L);
-        user.setUsername("antonio");
-        user.setEmail("antonio@gmail.com");
-        user.setRole(UserRole.USER);
+                User user = new User();
+                user.setId(1L);
+                user.setUsername("antonio");
+                user.setEmail("antonio@gmail.com");
+                user.setRole(UserRole.USER);
 
-        when(userRepository.findById(1L))
-                .thenReturn(Optional.of(user));
+                when(userRepository.findById(1L))
+                                .thenReturn(Optional.of(user));
 
-        when(userRepository.existsByEmailAndIdNot("paula@gmail", 1L))
-                .thenReturn(true);
+                when(userRepository.existsByEmailAndIdNot("paula@gmail", 1L))
+                                .thenReturn(true);
 
-        assertThrows(EmailAlreadyExistsException.class,
-                () -> userService.updateUser(1L, new UpdateUserDTO("Paulo", "paula@gmail")));
-    }
+                assertThrows(EmailAlreadyExistsException.class,
+                                () -> userService.updateUser(1L, new UpdateUserDTO("Paulo", "paula@gmail"),
+                                                UserRole.ADMIN.name()));
+        }
 
-    @Test
-    @DisplayName("Deve excluir usuário")
-    void deleteUserCase1() {
+        @Test
+        @DisplayName("Deve excluir usuário")
+        void deleteUserCase1() {
 
-        User user = new User();
-        user.setId(1L);
-        user.setRole(UserRole.USER);
+                User user = new User();
+                user.setId(1L);
+                user.setRole(UserRole.USER);
 
-        when(userRepository.findById(1L))
-                .thenReturn(Optional.of(user));
+                when(userRepository.findById(1L))
+                                .thenReturn(Optional.of(user));
 
-        userService.deleteUser(1L, "ROLE_ADMIN");
+                userService.deleteUser(1L, "ROLE_ADMIN");
 
-        verify(userRepository).delete(user);
-    }
+                verify(userRepository).delete(user);
+        }
 
-    @Test
-    @DisplayName("Deve lançar exceção quando tentar excluir admin")
-    void deleteUserCase2() {
+        @Test
+        @DisplayName("Deve lançar exceção quando tentar excluir admin")
+        void deleteUserCase2() {
 
-        User user = new User();
-        user.setId(1L);
-        user.setUsername("antonio");
-        user.setRole(UserRole.ADMIN);
+                User user = new User();
+                user.setId(1L);
+                user.setUsername("antonio");
+                user.setRole(UserRole.ADMIN);
 
-        when(userRepository.findById(1L))
-                .thenReturn(Optional.of(user));
+                when(userRepository.findById(1L))
+                                .thenReturn(Optional.of(user));
 
-        assertThrows(
-                UserForbiddenException.class,
-                () -> userService.deleteUser(1L, "ROLE_ADMIN"));
-    }
+                assertThrows(
+                                UserForbiddenException.class,
+                                () -> userService.deleteUser(1L, "ROLE_ADMIN"));
+        }
 
-    @Test
-    @DisplayName("Deve lançar exceção quando tentar excluir e não for admin autorizado")
-    void deleteUserCase3() {
+        @Test
+        @DisplayName("Deve lançar exceção quando tentar excluir e não for admin autorizado")
+        void deleteUserCase3() {
 
-        User user = new User();
-        user.setId(1L);
-        user.setUsername("antonio");
-        user.setRole(UserRole.USER);
+                User user = new User();
+                user.setId(1L);
+                user.setUsername("antonio");
+                user.setRole(UserRole.USER);
 
-        when(userRepository.findById(1L))
-                .thenReturn(Optional.of(user));
+                when(userRepository.findById(1L))
+                                .thenReturn(Optional.of(user));
 
-        assertThrows(
-                UserForbiddenException.class,
-                () -> userService.deleteUser(1L, "ROLE_USER"));
-    }
+                assertThrows(
+                                UserForbiddenException.class,
+                                () -> userService.deleteUser(1L, "ROLE_USER"));
+        }
 
-    @Test
-    @DisplayName("Deve lançar exceção quando usuário não existir")
-    void findByIdCase1() {
+        @Test
+        @DisplayName("Deve lançar exceção quando usuário não existir")
+        void findByIdCase1() {
 
-        when(userRepository.findById(1L))
-                .thenReturn(Optional.empty());
+                when(userRepository.findById(1L))
+                                .thenReturn(Optional.empty());
 
-        assertThrows(
-                UserNotFoundException.class,
-                () -> userService.findById(1L));
-    }
+                assertThrows(
+                                UserNotFoundException.class,
+                                () -> userService.findById(1L));
+        }
 }
